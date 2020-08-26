3e04edf9acafd413f3ce89cb2a75a5af09499ad1
Theodore Hui <tchui@ucsd.edu>
2020-08-26 21:46:40 +0000

    completed up to Part 2

--- hello_Sidharth.py
+++ hello_Sidharth.py
@@ -1,57 +1,88 @@
 from flask import Flask
+from flask import Flask, url_for, render_template, request
+
 
 app = Flask(__name__)
 
-
-
 @app.route("/")
+def render_home():
 
-def hello():
+   return  render_template('home.html')
 
-   return "Hello World!"
+@app.route('/ctof')
+def render_ctof():
 
+    return render_template('ctof.html')
+
+@app.route('/ftoc')
+def render_ftoc():
+
+    return render_template('ftoc.html')
+
+@app.route('/mtokm')
+def render_mtokm():
+
+    return render_template('mtokm.html')
+
+ 
+
+@app.route('/ftoc-result')
+def render_ftoc_result():
+
+    try:
+
+        ftemp_result = float(request.args['ftemp'])
+
+        ctemp_result = ftoc(ftemp_result)
+
+        return render_template('ftoc_result.html', ftemp=ftemp_result, ctemp=ctemp_result)
+
+    except ValueError:
+
+        return "Sorry: something went wrong."
+
+
+
+@app.route('/ctof-result')
+def render_ctof_result():
+
+    try:
+
+        ctemp_result = float(request.args['ctemp'])
+
+        ftemp_result = ctof(ctemp_result)
+
+        return render_template('ctof_result.html', ctemp=ctemp_result, ftemp=ftemp_result)
+
+    except ValueError:
+
+        return "Sorry: something went wrong."
+
+
+
+@app.route('/mtokm-result')
+def render_mtokm_result():
+
+    try:
+
+        miles_result = float(request.args['mdist'])
+
+        kilometers_result = miles_to_km(miles_result)
+
+        return render_template('mtokm_result.html', mdist = miles_result, kmdist = kilometers_result)
+
+    except ValueError:
+
+        return "Sorry: something went wrong."
+
+def ctof(ctemp):
+    return (ctemp*9/5)+32
 def ftoc(ftemp):
-
    return (ftemp - 32.0) * (5.0 / 9.0)
 
-@app.route('/ftoc/<ftemp_str>')
-def convert_ftoc(ftemp_str):
-
-    ftemp = 0.0
-
-    try:
-
-        ftemp = float(ftemp_str)
-
-        ctemp = ftoc(ftemp)
-
-        return "In Farenheit: " + ftemp_str + " In Celsius " + str(ctemp) 
-
-    except ValueError:
-
-        return "Sorry.  Could not convert " + ftemp_str + " to a number"
-
 def miles_to_km(miles):
-
     return (miles*1.60934)
 
-@app.route('/mtokm/<miles_str>')
-def convert_mtokm(miles_str):
-    miles = 0.0
-
-    try:
-        miles = float(miles_str)
-
-        kilometers = miles_to_km(miles)
-
-        kilometers = int(kilometers)
-
-        return f'{miles} miles is {kilometers} kilometers'
-    except ValueError:
-        return "Sorry.  Could not convert " + miles_str + " to a number"
-
-
 if __name__ == "__main__":
-
    app.run(host='0.0.0.0')
    print(f'done')
--- hello_Theodore.py
+++ hello_Theodore.py
@@ -1,14 +1,97 @@
-from flask import Flask
+import os
+
+from flask import Flask, url_for, render_template, request
+
+
 
 app = Flask(__name__)
 
 
 
-@app.route("/")
+@app.route('/')
 
-def hello():
+def render_main():
 
-   return "Hello World! (Theo)"
+    return render_template('home.html')
+
+
+
+@app.route('/ctof')
+
+def render_ctof():
+
+    return render_template('ctof.html')
+
+
+
+@app.route('/ftoc')
+
+def render_ftoc():
+
+    return render_template('ftoc.html')
+
+
+
+@app.route('/mtokm')
+
+def render_mtokm():
+
+    return render_template('mtokm.html')
+
+    
+
+@app.route('/ftoc-result')
+
+def render_ftoc_result():
+
+    try:
+
+        ftemp_result = float(request.args['ftemp'])
+
+        ctemp_result = ftoc(ftemp_result)
+
+        return render_template('ftoc_result.html', ftemp=ftemp_result, ctemp=ctemp_result)
+
+    except ValueError:
+
+        return "Sorry: something went wrong."
+
+
+
+@app.route('/ctof-result')
+
+def render_ctof_result():
+
+    try:
+
+        ctemp_result = float(request.args['ctemp'])
+
+        ftemp_result = ctof(ctemp_result)
+
+        return render_template('ctof_result.html', ctemp=ctemp_result, ftemp=ftemp_result)
+
+    except ValueError:
+
+        return "Sorry: something went wrong."
+
+
+
+@app.route('/mtokm-result')
+
+def render_mtokm_result():
+
+    try:
+
+        mdist_result = float(request.args['mdist'])
+
+        kmdist_result = miles_to_km(mdist_result)
+
+        return render_template('mtokm_result.html', kmdist=kmdist_result, mdist=mdist_result)
+        return render_template('mtokm.html')
+
+    except ValueError:
+
+        return "Sorry: something went wrong."
 
 
 
@@ -16,43 +99,20 @@ def ftoc(ftemp):
 
    return (ftemp - 32.0) * (5.0 / 9.0)
 
-@app.route('/ftoc/<ftemp_str>')
+    
 
-def convert_ftoc(ftemp_str):
+def ctof(ctemp):
 
-    ftemp = 0.0
-
-    try:
-
-        ftemp = float(ftemp_str)
-
-        ctemp = ftoc(ftemp)
-
-        return "In Farenheit: " + ftemp_str + " In Celsius " + str(ctemp) 
-
-    except ValueError:
-
-        return "Sorry.  Could not convert " + ftemp_str + " to a number"
+    return (ctemp *9/5) + 32
 
 
-@app.route('/mtokm/<mdist_str>')
+def miles_to_km(miles):
+    return (miles*1.60934)
 
-def convert_mtokm(mdist_str):
+# You'll probably want a basic function here to convert miles to kilometers too...
 
-    ftemp = 0.0
+    
 
-    try:
+if __name__=="__main__":
 
-        mdist = float(mdist_str)
-
-        kmdist = ftoc(mdist)
-
-        return "In Miles: " + mdist_str + " In Kilometers " + str(kmdist) 
-
-    except ValueError:
-
-        return "Sorry.  Could not convert " + mdist_str + " to a number"
-
-if __name__ == "__main__":
-
-   app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
